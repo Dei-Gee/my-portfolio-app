c9a5521ce0cf8adc42265ea03af6eea186ae0d6b
MacbOOK <macbook@MacbOOKs-MacBook.local>
2018-11-28 23:43:58 -0800

    mobile optimization 30% done...

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -3,33 +3,43 @@ import React, { Component } from 'react'
 import './Header.css';
 import Logo from '../Body/images/logo alone.png';
 import {NavLink} from 'react-router-dom';
-
+ 
 class Header extends Component{
     render(){
         return(
             <div className="header-container">
-                <div className="logo">
-                    <NavLink to="/" title="dejiojo.me"><img src={Logo} className="deji-logo" alt="logo" /></NavLink>
-                </div>
-                <div className="nav-container">
-                    <div className="links-container">
-                        <div className="nav-div">
-                            <NavLink className="links" to="/">Home</NavLink>
-                            <div className="bottom"></div>
-                        </div>
-                        <div className="nav-div">
-                            <NavLink className="links" to="/about">About</NavLink>
-                            <div className="bottom"></div>
-                        </div>
-                        <div className="nav-div">
-                            <NavLink className="links" to="/portfolio">Portfolio</NavLink>
-                            <div className="bottom"></div>
-                        </div>
-                        <div className="nav-div" id="last">
-                            <NavLink className="links" to="/contact">Contact</NavLink>
-                            <div className="bottom"></div>
+                <div className="desktop-header">
+                    <div className="logo">
+                        <NavLink to="/" title="dejiojo.me"><img src={Logo} className="deji-logo" alt="logo" /></NavLink>
+                    </div>
+                    <div className="nav-container">
+                        <div className="links-container">
+                            <div className="nav-div">
+                                <NavLink className="links" to="/">Home</NavLink>
+                                <div className="bottom"></div>
+                            </div>
+                            <div className="nav-div">
+                                <NavLink className="links" to="/about">About</NavLink>
+                                <div className="bottom"></div>
+                            </div>
+                            <div className="nav-div">
+                                <NavLink className="links" to="/portfolio">Portfolio</NavLink>
+                                <div className="bottom"></div>
+                            </div>
+                            <div className="nav-div" id="last">
+                                <NavLink className="links" to="/contact">Contact</NavLink>
+                                <div className="bottom"></div>
+                            </div>
                         </div>
                     </div>
+                </div>
+
+                <div className="mobile-header">
+                    <div className="logo">
+                        <NavLink to="/" title="dejiojo.me"><img src={Logo} className="deji-logo" alt="logo" /></NavLink>
+                    </div>
+
+                    
                 </div>
             </div>
         );
